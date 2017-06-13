db0c0d808c2527247926349c944577880d7b2ea0
Rachel Feldman <feldmanrm@g.cofc.edu>
2017-06-13 18:37:20 -0400

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,8 @@
+<form action="https://system.spektrix.com/<clientname>/website/secure/signup.aspx"
+method="POST">
+<input type="hidden" name="ReturnUrl" value="http://clientwebsite.org/signup.php" />
+First name: <input name="FirstName" type="text" />
+Last name: <input name="LastName" type="text" />
+Email address: <input name="Email" type="text" />
+<input type="submit" value="Signup" />
+</form>
